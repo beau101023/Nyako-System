847c324f9e7ee1410961c054214745b8fed38605
beau101023 <bbaer10000@gmail.com>
2023-09-24 20:04:18 -0600

    moved nyako files to nyako folder

--- nyako_voice_testing.py
+++
@@ -1,3 +0,0 @@
-from nyako_tts import say
-
-say("Heck! I'm a catgirl!", model_reload=True)
--- nyako_warmup.py
+++
@@ -1,6 +0,0 @@
-import nyako_tts
-import nyako_te
-
-def warmup():
-    nyako_tts.warmup()
-    nyako_te.warmup()
