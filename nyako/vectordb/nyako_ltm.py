e0b880a8ea64b2e7b605530e1486c50544377080
beau101023 <bbaer10000@gmail.com>
2023-10-05 11:58:33 -0600

    fixed long term memory api

--- nyako/vectordb/nyako_ltm.py
+++ nyako/vectordb/nyako_ltm.py
@@ -1,15 +1,15 @@
 from docarray.index import HnswDocumentIndex
-from data_schema import MemoryMessage
+from docarray import DocList
+from vectordb.data_schema import MemoryMessage
 from openai import Embedding
 from nyako_params import API_KEY
-
-openai.api_key = API_KEY
+from nyako_params import similarity_threshold
 
 # workspace path
-similarityIndex = HnswDocumentIndex[MemoryMessage](work_dir='./nyako/vectordb/workspace_path')
+similarityIndex = HnswDocumentIndex[MemoryMessage](work_dir='./vectordb/workspace_path')
 
 def insertToMemory(message: str):
-    embedding = Embedding.create(message, model="text-embedding-ada-002")['data'][0]['embedding']
+    embedding = Embedding.create(api_key=API_KEY, input=message, model="text-embedding-ada-002")['data'][0]['embedding']
     similarityIndex.index(DocList[MemoryMessage]([MemoryMessage(text=message, insertionOrdinal=similarityIndex.num_docs(), embedding=embedding)]))
 
 def retrieveMemoriesWithContext(message: str, memoriesToRetrieve: int, contextSize: int):
@@ -18,21 +18,27 @@ def retrieveMemoriesWithContext(message: str, memoriesToRetrieve: int, contextSi
     for memory in memories:
         memoriesWithContext.append(getMemoryContext(memory.insertionOrdinal, contextSize))
 
+    return memoriesWithContext
+
 # returns a list of memories that are similar to the query
 def queryMemory(toSearch: str, limit: int):
-    embedding = Embedding.create(toSearch, model="text-embedding-ada-002")['data'][0]['embedding']
+    embedding = Embedding.create(api_key=API_KEY, input=toSearch, model="text-embedding-ada-002")['data'][0]['embedding']
     # insertionOrdinal is ignored here
     query = MemoryMessage(text=toSearch, insertionOrdinal=0, embedding=embedding)
     results, scores = similarityIndex.find(query, limit=limit, search_field='embedding')
+
+    # reject results below the similarity threshold based on the scores returned by the search
+    results = [result for result, score in zip(results, scores) if score > similarity_threshold]
+
     return results
 
 # retrieves a 'context' around the memory at the given ordinal, including the memory at the given ordinal
 def getMemoryContext(memoryOrdinal: int, maxDistance: int):
     query = {
-        '$and': [{'sID': {'$lte': memoryOrdinal+maxDistance}}, {'sID': {'$gte': memoryOrdinal-maxDistance}}]
+        '$and': [{'insertionOrdinal': {'$lte': memoryOrdinal+maxDistance}}, {'insertionOrdinal': {'$gte': memoryOrdinal-maxDistance}}]
     }
 
-    results = similarityIndex.filter(query, limit=maxDistance*2)
+    results = similarityIndex.filter(query, limit=maxDistance*2+1)
 
     # sort results by insertionOrdinal
     results.sort(key=lambda x: x.insertionOrdinal)
