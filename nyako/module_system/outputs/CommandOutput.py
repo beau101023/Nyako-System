df724a6ba893ff5cce7c2ec5deab056c724f4f5b
beau101023 <bbaer10000@gmail.com>
2023-12-21 23:14:09 -0700

    Moved functionality out of CommandOutput into SleepManager so other things can sleep the system now

--- nyako/module_system/outputs/CommandOutput.py
+++ nyako/module_system/outputs/CommandOutput.py
@@ -11,8 +11,7 @@ class CommandOutput():
         self.sleep_length = 60*60 # 1 hour
         self.event_bus = event_bus
         self.event_bus.subscribe(self.onSleepingTriggered, Topics.Router.SLEEP)
-        self.event_bus.subscribe(self.onWake, Topics.System.WAKE)
-        self.event_bus.subscribe(self.onStopTriggered, Topics.Router.STOP)
+        self.event_bus.subscribe(self.onStopTriggered, Topics.Router.SHUTDOWN)
         self.wake_event = asyncio.Event()
         return self
     
@@ -39,22 +38,6 @@ class CommandOutput():
 
     async def onSleepingTriggered(self, text: str):
         await self.event_bus.publish(Topics.System.SLEEP)
-
-        _ = asyncio.create_task(self.wakeLaterTask())
-
-    async def wakeLaterTask(self):
-        # wakes after sleep_length seconds or when wake_event is set
-        try:
-            await asyncio.wait_for(self.wake_event.wait(), timeout=self.sleep_length)
-        except asyncio.TimeoutError:
-            # if the wake event isn't set, wake up
-            await self.event_bus.publish(Topics.System.WAKE)
-        finally:
-            # if the wake event was set, Topics.System.WAKE was already published
-            self.wake_event.clear()
-
-    async def onWake(self):
-        self.wake_event.set()
     
     async def onListeningTriggered(self, text: str):
         pass
