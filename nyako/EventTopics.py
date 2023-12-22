2815725fc19ff89a2931b814a847c0898ca38a57
beau101023 <bbaer10000@gmail.com>
2023-12-21 23:04:09 -0700

    Add new event topics Support publishing messages to all outputs

--- nyako/EventTopics.py
+++ nyako/EventTopics.py
@@ -13,17 +13,29 @@ class Topics:
         starting: bool = False
         ending: bool = False
 
+    @dataclass
+    class VolumeUpdate:
+        volume: float
+
     class TTS:
         SPEAKING_STATE = 'TTS_speaking_state'
 
+    class SpeechToText:
+        USER_SPEAKING_STATE = 'speech_to_text_speaking_state'
+
     class Router:
-        STOP = 'message_router[stop]'
-        LISTENING = 'message_router[listening]'
+        SHUTDOWN = 'message_router[stop]'
+        LISTEN = 'message_router[listen]'
         SLEEP = 'message_router[sleep]'
         VOICE = 'message_router[voice]'
         CONSOLE = 'message_router[console]'
         DISCORD = 'message_router[discord]'
         ERROR = 'message_router_error'
+
+        class Outputs:
+            CONSOLE = 'message_router[console]'
+            DISCORD = 'message_router[discord]'
+            VOICE = 'message_router[voice]'
 
     @dataclass
     class OutputStateUpdate:
@@ -32,6 +44,10 @@ class Topics:
 
     class Discord:
         LISTENING_CHANNEL_SET = 'discord_listening_channel_set'
+
+    class Audio:
+        INPUT_VOLUME_UPDATE = 'audio_input_volume_update'
+        OUTPUT_VOLUME_UPDATE = 'audio_output_volume_update'
 
     class System:
         OUTPUT_STATE = 'output_state'
