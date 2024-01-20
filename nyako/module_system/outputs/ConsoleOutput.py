af1a5dfe306f9a7ca78002f93422021c779d4962
beau101023 <bbaer10000@gmail.com>
2024-01-20 10:35:31 -0700

    Add event bus subscription to ConsoleOutput class

--- nyako/module_system/outputs/ConsoleOutput.py
+++ nyako/module_system/outputs/ConsoleOutput.py
@@ -2,12 +2,15 @@ from EventTopics import Topics
 
 class ConsoleOutput:
     @classmethod
-    async def create(self, event_bus):
+    async def create(self, event_bus, listen_topic):
         self = ConsoleOutput()
         self.tag = "console"
         self.event_bus = event_bus
         stateUpdate = Topics.OutputStateUpdate(self.tag, True)
         await self.event_bus.publish(Topics.System.OUTPUT_STATE, stateUpdate)
+        
+        self.event_bus.subscribe(self.onMessage, listen_topic)
+
         return self
 
     async def onMessage(self, message: str):
