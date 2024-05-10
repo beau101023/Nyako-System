beaa4257a8fd5469e9fb558bf0a8edfd21cdf6b8
beau101023 <bbaer10000@gmail.com>
2024-05-09 22:46:20 -0600

    Refactor speech transcription module to add support for multiple transcriber implementations

--- nyako/nyako_stt.py
+++ nyako/nyako_stt.py
@@ -1,22 +1,115 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 
-from ocotillo.api import Transcriber
+import whisper_at as whisper
+from ocotillo.api import Transcriber as OcotilloSTT
+
 from params import device
 from params import INPUT_SAMPLING_RATE
 
 from threading import Thread
 from queue import Queue
 
-# accepts a bytes object containing raw audio data
-# returns a string containing the decoded text
-def transcribeSpeech(speechBuffer, transcriber: Transcriber, input_gain=1.0):
-    speechBufferNumPyArray = np.fromstring(speechBuffer, dtype=np.float32)
+from typing import List
 
-    # apply input gain
-    speechBufferNumPyArray *= input_gain
+class Transcriber(ABC):
+    def __init__(self):
+        pass
 
-    # returns decoded text
-    return transcriber.transcribe(speechBufferNumPyArray, sample_rate=INPUT_SAMPLING_RATE)
+    @abstractmethod
+    def transcribeSpeech(self, speechBuffer, input_gain=1.0) -> str:
+        """
+        Transcribes a section of audio data using the provided transcriber object, with the specified input gain.
+
+        Parameters:
+        speechBuffer (bytes): a bytes object containing the audio data
+        input_gain (float): a float representing the input gain. Default is 1.0.
+
+        Returns:
+        Transcription of the speech.
+        """
+
+    @abstractmethod
+    def supports_extra_tagging(self) -> bool:
+        """
+        Returns whether the transcriber supports extra labels.
+
+        Returns:
+        bool: whether the transcriber supports extra labels
+        """
+
+    @abstractmethod
+    def get_extra_tagging(self) -> List[str]:
+        """
+        Returns the extra labels supported by the transcriber.
+
+        Returns:
+        list: the extra labels supported by the transcriber
+        """
+
+class WhisperTranscriber(Transcriber):
+    def __init__(self, no_speech_probability_threshold:float = 0.7):
+        self.transcriber = whisper.load_model("large-v1", device=device, in_memory=True)
+        self.no_speech_probability_threshold = no_speech_probability_threshold
+        self.result = None
+
+    def transcribeSpeech(self, speechBuffer, input_gain=1.0):
+        speechBufferNumPyArray = np.fromstring(speechBuffer, dtype=np.float32)
+
+        # apply input gain
+        speechBufferNumPyArray *= input_gain
+
+        # run transcription
+        self.result = self.transcriber.transcribe(speechBufferNumPyArray, at_time_res=2, initial_prompt="Glossary: Nyako, Beau, based, ayo, cringe")
+
+        out_text = ""
+        for segment in self.result['segments']:
+            if(segment['no_speech_prob'] <= self.no_speech_probability_threshold):
+                out_text += segment['text']
+
+        return out_text
+    
+    def supports_extra_tagging(self):
+        return True
+    
+    def get_extra_tagging(self):
+        audio_tag_result = whisper.parse_at_label(self.result, top_k=2, p_threshold=-2)
+    
+        tags = set()
+        for segment in audio_tag_result:
+            for segment_tuple in segment['audio tags']:
+                tags.add(segment_tuple[0])  # Extract the tag from the tuple
+    
+        return list(tags)  # Convert the set to a list
+
+class OcotilloTranscriber(Transcriber):
+    def __init__(self):
+        self.transcriber = OcotilloSTT(on_cuda=device == 'cuda')
+
+    def transcribeSpeech(self, speechBuffer, input_gain=1.0):
+        speechBufferNumPyArray = np.fromstring(speechBuffer, dtype=np.float32)
+
+        # apply input gain
+        speechBufferNumPyArray *= input_gain
+
+        # returns decoded text
+        return self.transcriber.transcribe(speechBufferNumPyArray, sample_rate=INPUT_SAMPLING_RATE)
+    
+    def supports_extra_tagging(self):
+        return False
+    
+    def get_extra_tagging(self):
+        return []
+    
+    def supports_speech_probability(self):
+        return False
+    
+    def get_speech_probability(self) -> float:
+        return 0.0
+
+from typing import Type
+from typing import Dict
 
 # transcriber object pool to avoid the overhead of spinning up a new transcriber on demand
 #  one transcriber is required for every audio stream because transcribers may be stateful
