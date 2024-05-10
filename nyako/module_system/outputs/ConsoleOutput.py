859a51514d77d77a9ec7fa748c686b44db7b98e5
beau101023 <bbaer10000@gmail.com>
2024-05-09 23:58:58 -0600

    Add docstrings to ConsoleOutput module

--- nyako/module_system/outputs/ConsoleOutput.py
+++ nyako/module_system/outputs/ConsoleOutput.py
@@ -1,8 +1,20 @@
 from EventTopics import Topics
 
 class ConsoleOutput:
+    """
+    Outputs messages to the console.
+    """
+
     @classmethod
     async def create(self, event_bus, listen_topic):
+        """
+        Creates an instance of the ConsoleOutput module.
+
+        Parameters:
+        event_bus (EventBus): the event bus to use
+        listen_topic (str): the topic to listen for messages on
+        """
+
         self = ConsoleOutput()
         self.tag = "console"
         self.event_bus = event_bus
@@ -14,4 +26,11 @@ class ConsoleOutput:
         return self
 
     async def onMessage(self, message: str):
+        """
+        Outputs a message to the console asynchonously.
+        
+        Parameters:
+        message (str): the message to output
+        """
+
         print("\n" + message)
