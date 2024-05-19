4ea320e602d8861ae01fdf6c21f497620ff05b58
beau101023 <bbaer10000@gmail.com>
2024-05-18 20:26:35 -0600

    Comment out unfinished class as import throws error

--- nyako/module_system/outputs/__init__.py
+++ nyako/module_system/outputs/__init__.py
@@ -2,7 +2,7 @@ from .command_output.CommandOutput import CommandOutput
 from .command_output.Command import Command
 from .ConsoleOutput import ConsoleOutput
 from .DiscordOutput import DiscordOutput
-from .DiscordVoiceOutput import DiscordVoiceOutput
+#from .DiscordVoiceOutput import DiscordVoiceOutput
 from .VisualOutput import VisualOutput
 
 ## Not imported by default due to need to run VSCode as administrator to use
