244eb247557939dc64691fe2143d78e4447742d5
beau101023 <bbaer10000@gmail.com>
2024-05-23 22:47:29 -0600

    Bring TaskManager up to spec with new event system

--- nyako/TaskManager.py
+++ nyako/TaskManager.py
@@ -1,20 +1,16 @@
 import asyncio
-from Events import Events
-from EventBus import EventBus
-from EventBusSingleton import EventBusSingleton
+from event_system.events.System import TaskCreatedEvent
+from nyako.event_system.EventBus import EventBus
+from nyako.event_system.EventBusSingleton import EventBusSingleton
 
 class TaskManager:
-    event_bus: EventBus
-
     def __init__(self):
         self.tasks = []
-        EventBusSingleton.get().subscribe(self.onTaskCreated, Events.System.TaskCreated)
+        EventBusSingleton.get().subscribe(self.onTaskCreated, TaskCreatedEvent)
 
-    def onTaskCreated(self, event: Events.System.TaskCreated):
-        task = event.task
-        
-        print("Task registered for " + str(task))
-        self.tasks.append(task)
+    def onTaskCreated(self, event: TaskCreatedEvent):
+        print("Task registered for " + str(event.pretty_sender))
+        self.tasks.append(event.task)
 
     async def run(self):
         await asyncio.gather(*self.tasks)
