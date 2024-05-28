c785d5dc3bac62edbdcbd9d19b009465e6b0743c
beau101023 <bbaer10000@gmail.com>
2024-05-28 17:26:13 -0600

    Use new singleton api, print when async shutdown successful

--- nyako/TaskManager.py
+++ nyako/TaskManager.py
@@ -1,12 +1,11 @@
 import asyncio
 from event_system.events.System import TaskCreatedEvent
-from nyako.event_system.EventBus import EventBus
-from nyako.event_system.EventBusSingleton import EventBusSingleton
+from event_system import EventBusSingleton
 
 class TaskManager:
     def __init__(self):
         self.tasks = []
-        EventBusSingleton.get().subscribe(self.onTaskCreated, TaskCreatedEvent)
+        EventBusSingleton.subscribe(TaskCreatedEvent, self.onTaskCreated)
 
     def onTaskCreated(self, event: TaskCreatedEvent):
         print("Task registered for " + str(event.pretty_sender))
@@ -14,3 +13,5 @@ class TaskManager:
 
     async def run(self):
         await asyncio.gather(*self.tasks)
+
+        print("All tasks gathered")
