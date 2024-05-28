3bef21d9e4e32bf1e71357ab3fb279a305df06bb
beau101023 <bbaer10000@gmail.com>
2024-05-28 17:56:50 -0600

    Make DiscordClientRunner run and publish a discord client

--- nyako/pipesys/core/DiscordClientRunner.py
+++ nyako/pipesys/core/DiscordClientRunner.py
@@ -1,22 +1,28 @@
 import asyncio
 import discord
+from event_system.events.Discord import BotReadyEvent
 from params import DISCORD_BOT_TOKEN
 from event_system.events.System import TaskCreatedEvent
 
-from nyako.event_system.EventBusSingleton import EventBusSingleton
+from event_system.EventBusSingleton import EventBusSingleton
 
 class DiscordClientRunner:
     def __init__(self):
-        self.client = discord.Client()
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+        self.client = discord.Client(intents=intents)
+        self.client.event(self.on_ready)
 
     @classmethod
     async def create(cls) -> 'DiscordClientRunner':
         self = DiscordClientRunner()
 
         task = asyncio.create_task(self.client.start(DISCORD_BOT_TOKEN))
-        await EventBusSingleton.publish(TaskCreatedEvent(task))
+        await EventBusSingleton.publish(TaskCreatedEvent(task, "Discord Client"))
 
         return self
     
-    def getClient(self):
-        return self.client
+    async def on_ready(self):
+        print("Discord Bot Connected")
+        await EventBusSingleton.publish(BotReadyEvent(self.client))
