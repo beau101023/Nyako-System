92a7f41f2a0985915e0c422b1a85362043476c03
beau101023 <bbaer10000@gmail.com>
2024-05-28 18:11:00 -0600

    Use new events api, harden typing

--- nyako/pipesys/inputs/DiscordInput.py
+++ nyako/pipesys/inputs/DiscordInput.py
@@ -1,39 +1,40 @@
 import discord
-from nyako.event_system.EventBus import EventBus
-from nyako.event_system.EventBusSingleton import EventBusSingleton
+from event_system.EventBusSingleton import EventBusSingleton
 
-from nyako.events.IO import SystemInputType, UserInputEvent
-from events.System import CommandEvent, CommandType, StartupStageEnum, StartupEvent
-from events.System import StartupStageEnum
+from event_system.events.Pipeline import SystemInputType, UserInputEvent
+from event_system.events.System import CommandEvent, CommandType, StartupStage, StartupEvent
+from event_system.events.System import StartupStage
 
-from events.Discord import TextChannelConnectedEvent
+from event_system.events.Discord import TextChannelConnectedEvent
+from pipesys import Pipe
 
-class DiscordInput:
-    event_bus: EventBus
+class DiscordInput(Pipe):
     client: discord.Client
 
     def __init__(self):
-        self.listeningChannel: discord.TextChannel = None
+        self.listeningChannel: discord.abc.MessageableChannel | None = None
 
     @classmethod
     async def create(cls, client: discord.Client):
         self = DiscordInput()
-        self.event_bus = EventBusSingleton.get()
         self.client = client
 
         # a part of the 'discord' package, separate from the event system which uses EventBus
         client.event(self.onMessage)
 
         EventBusSingleton.subscribe(CommandEvent(CommandType.STOP), self.onStop)
-        EventBusSingleton.subscribe(StartupEvent(StartupStageEnum.WARMUP), self.onWarmup)
+        EventBusSingleton.subscribe(StartupEvent(StartupStage.WARMUP), self.onWarmup)
 
         return self
 
-    async def onWarmup(self):
+    async def onWarmup(self, event: StartupEvent):
         if(self.listeningChannel == None):
             await EventBusSingleton.publish(CommandEvent(CommandType.SLEEP))
 
     async def onMessage(self, message: discord.Message):
+        if self.client.user == None:
+            return
+
         if message.author.id == self.client.user.id:
             return
 
@@ -45,8 +46,8 @@ class DiscordInput:
         if(message.channel != self.listeningChannel):
             return
         
-        inputEvent = UserInputEvent(message.content, SystemInputType.DISCORD, user_name= message.author.name)
-        await self.event_bus.publish(inputEvent)
+        inputEvent = UserInputEvent(message.content, self, SystemInputType.DISCORD, user_name= message.author.name)
+        await EventBusSingleton.publish(inputEvent)
 
-    async def onStop(self):
+    async def onStop(self, event: CommandEvent):
         await self.client.close()
