58440f32c72120ca3d9ddbe620d5afa3728060c1
beau101023 <bbaer10000@gmail.com>
2024-05-28 21:44:59 -0600

    Update object creation, migrate to new event system, simplify chunk_messages, implement priority for inputs

--- nyako/pipesys/processors/RealtimeMessageChunker.py
+++ nyako/pipesys/processors/RealtimeMessageChunker.py
@@ -1,44 +1,49 @@
 import asyncio
-from datetime import datetime, timedelta
-from EventTopics import Topics
-from EventBus import EventBus
+from datetime import datetime
+
+from event_system import EventBusSingleton
+from event_system.events.System import TaskCreatedEvent, CommandEvent, CommandType
+from event_system.events.Pipeline import UserInputEvent, MessageEvent
+from event_system.events.Audio import SpeakingStateUpdate, AudioType, AudioDirection
+
+from pipesys import Pipe
 
 from params import default_processor_delay
 from params import default_no_input_interval_seconds
 
-class RealtimeMessageChunker:
-    event_bus: EventBus
-    stopped: bool = False
+class RealtimeMessageChunker(Pipe):
+    no_input_interval_seconds: int
+    processor_delay: int
     
+    def __init__(self):
+        super().__init__()
+        self.sleeping: bool = False
+        self.paused: bool = False
+        self.stopped: bool = False
+
+        self.last_input_time: datetime = datetime.now()
+        self.event_queue: list[UserInputEvent] = []
+        self.last_no_input_sent_time: datetime = datetime.now()
+
     # processor_delay is the amount of time to wait after the last message before processing the messages
     # no_input_interval_seconds is the amount of time to wait before sending a message indicating that there has been no input
     @classmethod
-    async def create(cls, event_bus, processor_delay: int = default_processor_delay, no_input_interval_seconds: int = default_no_input_interval_seconds, listen_topic=Topics.Pipeline.USER_INPUT, send_topic=Topics.Pipeline.CHUNKER):
+    async def create(cls, processor_delay: int = default_processor_delay, no_input_interval_seconds: int = default_no_input_interval_seconds):
         self = RealtimeMessageChunker()
-        self.event_bus = event_bus
-        self.send_topic = send_topic
         
-        self.task = asyncio.create_task(self.chunk_messages())
-        await self.event_bus.publish(Topics.System.TASK_CREATED, self.task)
+        task = asyncio.create_task(self.chunk_messages())
+        await EventBusSingleton.publish(TaskCreatedEvent(task, pretty_sender="Message Chunker"))
 
-        self.sleeping = False
-        self.paused = False
+        EventBusSingleton.subscribe(UserInputEvent, self.onMessage)
+        EventBusSingleton.subscribe(CommandEvent(CommandType.SLEEP), self.onSleep)
+        EventBusSingleton.subscribe(CommandEvent(CommandType.WAKE), self.onWake)
+        EventBusSingleton.subscribe(CommandEvent(CommandType.STOP), self.onStop)
+        EventBusSingleton.subscribe(SpeakingStateUpdate(audio_direction=AudioDirection.INPUT), self.onUserSpeakingStateUpdate)
 
-        self.event_bus.subscribe(self.onMessagePriority, listen_topic)
-        self.event_bus.subscribe(self.onSleep, Topics.System.SLEEP)
-        self.event_bus.subscribe(self.onWake, Topics.System.WAKE)
-        self.event_bus.subscribe(self.onStop, Topics.System.STOP)
-        self.event_bus.subscribe(self.onUserSpeakingStateUpdate, Topics.SpeechToText.USER_SPEAKING_STATE)
-
-        # make sure the LLM gets error feedback
-        self.event_bus.subscribe(self.onMessage, Topics.Router.ERROR)
+        # TODO make sure the LLM gets error feedback by subscribing to the LLMOutput events as well
 
         self.no_input_interval_seconds = no_input_interval_seconds
-        self.processor_delay: float = processor_delay
-
-        self.last_input_time = datetime.now()
-        self.messages = []
-        self.last_no_input_sent_time = datetime.now()
+        self.processor_delay = processor_delay
 
         return self
 
@@ -49,77 +54,104 @@ class RealtimeMessageChunker:
                 await asyncio.sleep(1)
                 continue
 
-            if self.messages_queued() and self.user_has_been_inactive_for_at_least(self.processor_delay):
+            if self.messages_queued() and self.user_inactivity_seconds() > self.processor_delay:
                 await self.process_messages()
 
                 # delay the next chunk of messages by the processor delay
                 self.last_input_time = datetime.now()
 
-            elif self.no_messages_queued() and self.user_has_been_inactive_for_at_least(self.no_input_interval_seconds):
-                if self.is_no_input_message_due():
-                    await self.send_no_input_message()
-                    self.last_no_input_sent_time = datetime.now()
+            elif (not self.messages_queued() and 
+                  self.user_inactivity_seconds() > self.no_input_interval_seconds and
+                  self.seconds_since_last_idle_response() > self.no_input_interval_seconds):
+                await self.send_no_input_message()
 
             await asyncio.sleep(1)
 
         # send any remaining messages
-        if len(self.messages) > 0:
+        if len(self.event_queue) > 0:
             await self.process_messages()
 
-    def is_no_input_message_due(self):
-        return datetime.now() >= self.last_no_input_sent_time + timedelta(seconds=self.no_input_interval_seconds)
+    def seconds_since_last_idle_response(self) -> float:
+        return (datetime.now() - self.last_no_input_sent_time).total_seconds()
 
-    async def send_no_input_message(self):
+    def user_inactivity_seconds(self) -> float:
+        return (datetime.now() - self.last_input_time).total_seconds()
+
+    async def send_no_input_message(self) -> None:
         time_since_last_input = str(datetime.now() - self.last_input_time).split(".")[0]
 
         messages = "[no input ({0}s)]".format(time_since_last_input)
 
+        self.last_no_input_sent_time = datetime.now()
+
         await self.send(messages)
 
-    def no_messages_queued(self):
-        return len(self.messages) == 0
+    def messages_queued(self) -> bool:
+        return len(self.event_queue) > 0
 
-    def user_has_been_inactive_for_at_least(self, seconds: int):
-        return datetime.now() - self.last_input_time > timedelta(seconds=seconds)
-
-    def messages_queued(self):
-        return len(self.messages) > 0
-
-    async def process_messages(self):
-        messages_to_process = self.messages
-        self.messages = []
+    async def process_messages(self) -> None:
+        messages_to_process = [str(event) for event in self.event_queue]
+        self.event_queue = []
 
         # join the messages into a string separated by newlines
-        messages = "\n".join(messages_to_process)
+        messages = "\n\n".join(messages_to_process)
 
         await self.send(messages)
 
     async def send(self, message: str):
-        await self.event_bus.publish(self.send_topic, message)
+        await EventBusSingleton.publish(MessageEvent(message, self))
 
-    async def onMessage(self, message: str):
+    async def onMessage(self, event: UserInputEvent):
+        if event.priority is None:
+            return
+
+        # don't add if `event` has a lower priority than other queued events
+        if event.priority is not None and self.queue_max_priority() > event.priority:
+            return
+
+        # past this point, we're actually queueing events, so wake up
         if(self.sleeping):
-            await self.event_bus.publish(Topics.System.WAKE)
-        self.messages.append(message)
+            await EventBusSingleton.publish(CommandEvent(CommandType.WAKE))
 
-    async def onMessagePriority(self, message: str):
-        self.last_input_time = datetime.now()
-        await self.onMessage(message)
+        # if `event` has a higher priority than all queued events, clear queue and add
+        if event.priority is not None and self.queue_max_priority() < event.priority:
+            self.event_queue = []
+            self.event_queue.append(event)
+            return
 
-    async def onSleep(self):
+        # event has the same priority, just add it
+        self.event_queue.append(event)
+
+    def queue_max_priority(self):
+        if len(self.event_queue) == 0:
+            return 0
+        
+        event_priorities: set[int] = set()
+
+        for message in self.event_queue:
+            if message.priority is None:
+                event_priorities.add(-1)
+            else:
+                event_priorities.add(message.priority)
+
+        return max(event_priorities)
+
+    async def onSleep(self, event: CommandEvent):
         self.sleeping = True
 
-    async def onWake(self):
+    async def onWake(self, event: CommandEvent):
         self.sleeping = False
 
-    async def onStop(self):
+    async def onStop(self, event: CommandEvent):
         self.stopped = True
 
-    async def onUserSpeakingStateUpdate(self, event: Topics.SpeakingStateUpdate):
-        # if the user is speaking, pause the chunker
-        # TODO: simplify the event object
-        if event.starting:
-            self.paused = True
-        elif event.ending:
-            self.paused = False
+    async def onUserSpeakingStateUpdate(self, event: SpeakingStateUpdate):
+        # if the user has finished a period of speech, update the last input time
+        if event.is_speaking == None:
+            return
+
+        if not event.is_speaking:
             self.last_input_time = datetime.now()
+
+        # if the user is speaking, pause the chunker
+        self.paused = event.is_speaking
