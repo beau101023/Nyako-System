d040ba94be58662545c0970cb897568072bb9bbf
beau101023 <bbaer10000@gmail.com>
2024-05-28 21:43:34 -0600

    Refactor ConversationSessionProcessor to use new event system and improve typing

--- nyako/pipesys/processors/ConversationSessionProcessor.py
+++ nyako/pipesys/processors/ConversationSessionProcessor.py
@@ -1,52 +1,62 @@
-from LLM.nyako_llm import ConversationSession
-import params
-from EventBus import EventBus
-from events.Event import Event
+from typing import Type
 
-class ConversationSessionProcessor:
+from overrides import override
+
+from LLM.nyako_llm import ConversationSession
+from nyako.pipesys.MessageReciever import MessageReceiver
+import params
+
+from event_system import EventBusSingleton
+from event_system.events.Pipeline import MessageEvent, OutputAvailabilityEvent, SystemOutputType
+from event_system.events.System import CommandEvent, CommandType
+from pipesys import Pipe
+
+class ConversationSessionProcessor(MessageReceiver):
     conversation_session: ConversationSession
-    tags: set[str]
-    event_bus: EventBus
+    available_outputs: set[SystemOutputType]
+
+    def __init__(self, listen_to):
+        super().__init__(listen_to)
 
     @classmethod
-    async def create(cls, event_bus, listen_to: Event):
-        self = ConversationSessionProcessor()
-        self.event_bus = event_bus
-        self.event_bus.subscribe(self.onOutputStateUpdate, Topics.System.OUTPUT_STATE)
-        self.event_bus.subscribe(self.onMessage, listen_topic)
-        self.event_bus.subscribe(self.onStop, Topics.System.STOP)
-        self.send_topic = send_topic
+    async def create(cls, listen_to: MessageEvent | Pipe | Type[MessageEvent]):
+        self = ConversationSessionProcessor(listen_to)
+
+        EventBusSingleton.subscribe(OutputAvailabilityEvent, self.onOutputStateUpdate)
+        EventBusSingleton.subscribe(CommandEvent(CommandType.STOP), self.onStop)
         
         # valid output tags
-        self.tags = set()
+        self.available_outputs = set()
 
         self.conversation_session = ConversationSession()
         self.conversation_session.updateSystemPrompt(self.getSystemPrompt())
 
         return self
 
-    async def onMessage(self, message: str):
+    @override
+    async def onMessage(self, event: MessageEvent):
         # this blocks the event loop, but we want to pause processing until we get a response anyway
-        response = self.conversation_session.query(message)
+        response = self.conversation_session.query(str(event))
 
-        await self.event_bus.publish(self.send_topic, response)
+        await EventBusSingleton.publish(MessageEvent(response, self))
 
-    async def onOutputStateUpdate(self, event: Topics.OutputStateUpdate):
-        if not event.tag in self.tags and not event.output_active:
+    async def onOutputStateUpdate(self, event: OutputAvailabilityEvent):
+        
+        if not event.output_type in self.available_outputs and not event.output_available:
             return
         
-        if(event.output_active):
-            self.tags.add(event.tag.lower())
+        if(event.output_available):
+            self.available_outputs.add(event.output_type)
         else:
-            self.tags.remove(event.tag.lower())
+            self.available_outputs.remove(event.output_type)
 
         self.conversation_session.updateSystemPrompt(self.getSystemPrompt())
 
     def getSystemPrompt(self):
-        if(len(self.tags) > 0):
-            return params.nyako_prompt + " Available outputs: [" + "], [".join(self.tags) + "]"
+        if(len(self.available_outputs) > 0):
+            return params.nyako_prompt + " Available outputs: [" + "], [".join([output.name.lower() for output in self.available_outputs]) + "]"
         else:
             return params.nyako_prompt
         
-    async def onStop(self):
+    async def onStop(self, event: CommandEvent):
         self.conversation_session.memorizeAll()
