d1e6bc7fe740cde5a7a7d0dd3ae3c087f7874a7d
beau101023 <bbaer10000@gmail.com>
2024-06-02 21:37:26 -0600

    implement MessageReciever, OutputPipe

--- nyako/pipesys/outputs/DiscordOutput.py
+++ nyako/pipesys/outputs/DiscordOutput.py
@@ -3,18 +3,19 @@ from event_system.EventBusSingleton import EventBusSingleton
 
 from event_system.events.Discord import TextChannelConnectedEvent
 from event_system.events.Pipeline import MessageEvent, OutputAvailabilityEvent, SystemOutputType
-from pipesys import Pipe
+from pipesys.MessageReciever import MessageReceiver
+from pipesys import Pipe, OutputPipe
 
-class DiscordOutput:
-    def __init__(self):
+class DiscordOutput(MessageReceiver, OutputPipe):
+    def __init__(self, listen_to: Pipe | MessageEvent | type[MessageEvent]):
+        super().__init__(listen_to)
         self.sendChannel: discord.abc.MessageableChannel | None = None
 
     @classmethod
-    async def create(cls, listen_to: Pipe):
-        self = DiscordOutput()
+    async def create(cls, listen_to: Pipe | MessageEvent | type[MessageEvent]):
+        self = DiscordOutput(listen_to)
 
         EventBusSingleton.subscribe(TextChannelConnectedEvent, self.set_channel)
-        EventBusSingleton.subscribe(MessageEvent(sender=listen_to), self.send_message)
 
         await EventBusSingleton.publish(OutputAvailabilityEvent(SystemOutputType.DISCORD, True))
         return self
@@ -22,7 +23,7 @@ class DiscordOutput:
     def set_channel(self, event: TextChannelConnectedEvent):
         self.sendChannel = event.channel
 
-    async def send_message(self, event: MessageEvent):
+    async def onMessage(self, event: MessageEvent):
         # discord will throw an error if the message is empty
         if event.message is None or not event.message.strip():
             return
