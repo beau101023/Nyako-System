2e53a5a6b371d3826d32e044b4de5b2dd87c7e96
beau101023 <bbaer10000@gmail.com>
2024-08-21 20:49:04 -0600

    Add PipelineMonitor and FileLogger modules for output monitoring and logging

--- nyako/pipesys/outputs/__init__.py
+++ nyako/pipesys/outputs/__init__.py
@@ -2,6 +2,8 @@ from .ConsoleOutput import ConsoleOutput
 from .DiscordOutput import DiscordOutput
 from .DiscordVoiceOutput import DiscordVoiceOutput
 from .VisualOutput import VisualOutput
+from .PipelineMonitor import PipelineMonitor
+from .FileLogger import FileLogger
 
 ## Not imported by default due to need to run VSCode as administrator to use
 #from .TextToSpeechOutput import TextToSpeechOutput
