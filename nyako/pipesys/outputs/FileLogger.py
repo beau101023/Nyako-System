675f88a2254e52d3d825ee6cdfefa377a863bf7d
beau101023 <bbaer10000@gmail.com>
2024-12-10 14:43:34 -0700

    Enhance FileLogger to listen for UserInputEvent and additional events specified in the constructor

--- nyako/pipesys/outputs/FileLogger.py
+++ nyako/pipesys/outputs/FileLogger.py
@@ -4,13 +4,15 @@ from datetime import datetime
 from overrides import override
 from event_system import EventBusSingleton
 from event_system.events.Pipeline import MessageEvent, OutputMessageEvent, UserInputEvent
-from event_system.events.System import CommandEvent, CommandType
-from pipesys.Pipe import OutputPipe
+from nyako.pipesys.MessageReciever import MessageReceiver
+from pipesys.Pipe import OutputPipe, Pipe
 from params import nyako_prompt
 
-class FileLogger(OutputPipe):
+class FileLogger(OutputPipe, MessageReceiver):
     """
     Module that logs messages to a file.
+    Automatically listens to all UserInputEvent.
+    Additionally, listens to an additional event or pipe specified in the constructor.
 
     The file is named "logs/log<current date and time>.txt".
 
@@ -21,23 +23,25 @@ class FileLogger(OutputPipe):
     assistant: <message>
     """
 
-    def __init__(self):
+    def __init__(self, listen_to):
+        super().__init__(listen_to)
+
         # Replace colons and spaces with underscores
         self.logfile_path = ("logs/log" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt")
         os.makedirs(os.path.dirname(self.logfile_path), exist_ok=True)
 
     @classmethod
-    async def create(cls):
-        self = FileLogger()
+    async def create(cls, listen_to: MessageEvent|Pipe|type[MessageEvent]):
+        self = FileLogger(listen_to)
 
         EventBusSingleton.subscribe(UserInputEvent, self.onMessage)
-        EventBusSingleton.subscribe(OutputMessageEvent, self.onMessage)
 
         async with aiofiles.open(self.logfile_path, mode='w') as logfile:
             await logfile.write(f"system: {nyako_prompt}\n")
 
         return self
 
+    @override
     async def onMessage(self, event: MessageEvent):
         """
         Logs received messages to a file.
