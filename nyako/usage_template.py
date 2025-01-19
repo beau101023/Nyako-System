6c7e9a8ecdb419f2275e665e71c58c313f121ae0
beau101023 <bbaer10000@gmail.com>
2025-01-18 17:14:28 -0700

    update usage template

--- nyako/usage_template.py
+++ nyako/usage_template.py
@@ -1,96 +1,106 @@
 import torch
-torch.set_num_threads(3)
+import qasync
 
 import Transcribers
 
+from event_system.EventBusSingleton import EventBusSingleton
+from event_system.events.System import StartupEvent, StartupStage
+from TTS import SileroRVC_TTS, SileroTTS
+from pipesys.outputs import ConsoleOutput, PipelineMonitor, DiscordOutput
+from pipesys.inputs import DiscordInput
+
+torch.set_num_threads(4)
+
 import asyncio
 
-import module_system
+import pipesys
+
+from event_system.events.Pipeline import MessageEvent, OutputMessageEvent, UserInputEvent
 
 ### NOTE: You need to run VSCode as administrator for this specific import to work due to stupid shit with TextToSpeechOutput's innerworkings
 ###     as such, it's not imported by default when you import module_system
-from module_system.outputs.TextToSpeechOutput import TextToSpeechOutput
+from pipesys.outputs.TextToSpeechOutput import TextToSpeechOutput
 
-from AdminEvents import AdminEvents
+from AdminPanel import AdminPanel
 from TaskManager import TaskManager
-
-from EventBus import EventBus
-from EventTopics import Topics
 
 async def main():
     # region Core Modules
 
-    ## MANDATORY The event bus is the central hub of the system.
-    event_bus = EventBus()
-
     ## MANDATORY Collects all tasks, must be created before task-producing modules
-    task_manager = TaskManager(event_bus)
+    task_manager = TaskManager()
 
     ## Handles the discord client
-    #discord_client_runner = await DiscordClientRunner.create(event_bus)
-    #discord_client = discord_client_runner.getClient()
+    discord_client_runner = await pipesys.core.DiscordClientRunner.create()
 
     ## Handles automated waking of the system after periods of inactivity
     ## Can be triggered by raising Topics.System.SLEEP
     ##  and woken prematurely by raising Topics.System.WAKE
     #sleep_manager = await SleepManager.create(event_bus)
 
-    ## Control panel ui
-    admin_events = await AdminEvents.create(event_bus, listen_topic=Topics.Pipeline.CONVERSATION_SESSION_REPLY)
+    admin_events = await AdminPanel.create(listen_to=OutputMessageEvent)
     
     # endregion
 
     # region Input Modules
 
     ## Multi-user voice input via discord
-    ## Joins the first voice channel available to the discord client
-    #discord_voice_input = await DiscordVoiceInput.create(event_bus, discord_client, publish_channel=Topics.Pipeline.USER_INPUT)
+    discord_voice_input = await pipesys.inputs.DiscordVoiceInput.create(Transcribers.WhisperTranscriber(no_speech_probability_threshold=0.6))
     
-    speech_to_text = await module_system.inputs.SpeechToTextInput.create(event_bus, transcriber=Transcribers.WhisperTranscriber(no_speech_probability_threshold=1.0), publish_channel=Topics.Pipeline.USER_INPUT)
+    ui_monitor = await PipelineMonitor.create(listen_to=UserInputEvent)
+    monitor = await PipelineMonitor.create(listen_to=MessageEvent)
+
+    #speech_to_text = await pipesys.inputs.SpeechToTextInput.create(Transcribers.WhisperTranscriber())
     #console_input = await ConsoleInput.create(event_bus)
-    #discord_input = await DiscordInput.create(event_bus, discord_client, publish_channel=Topics.Pipeline.USER_INPUT)
+    #discord_input = await DiscordInput.create()
     
     # endregion
 
     # region Processing Modules
 
     ## The chunker accumulates messages over a time period and sends them to the next processor as a batch
-    message_chunker = await module_system.processors.RealtimeMessageChunker.create(event_bus, listen_topic=Topics.Pipeline.USER_INPUT, send_topic=Topics.Pipeline.CHUNKER)
+    message_chunker = await pipesys.processors.RealtimeMessageChunker.create(listen_to=UserInputEvent, processor_delay=0.3)
     
     ## The conversation session processor queries the LLM
-    conversation_session_processor = await module_system.processors.ConversationSessionProcessor.create(event_bus, listen_topic=Topics.Pipeline.CHUNKER, send_topic=Topics.Pipeline.CONVERSATION_SESSION_REPLY)
+    conversation_session_processor = await pipesys.processors.ConversationSessionProcessor.create(listen_to=message_chunker)
     
     ## The message router sends the results to the output modules based on a tagging system
     ## For example, if the LLM produces an output with the string "[voice]", the message router will send the text after that tag to the voice output module
-    #message_router = MessageRouter(event_bus, listen_topic=Topics.Pipeline.CONVERSATION_SESSION_REPLY)
+    #message_router = pipesys.processors.MessageRouter(listen_to=conversation_session_processor)
 
     # endregion
 
     # region Output Modules
 
-    console_output = await module_system.outputs.ConsoleOutput.create(event_bus, listen_topic=Topics.Pipeline.CONVERSATION_SESSION_REPLY)
-    speech_output = await TextToSpeechOutput.create(event_bus, listen_topic=Topics.Pipeline.CONVERSATION_SESSION_REPLY)
+    #console_output = await pipesys.outputs.ConsoleOutput.create(listen_to=conversation_session_processor)
+    #speech_output = await TextToSpeechOutput.create(listen_to=conversation_session_processor)
+    discord_voice_output = await pipesys.outputs.DiscordVoiceOutput.create(listen_to=conversation_session_processor, speech_to_text=SileroTTS())
 
-    user_input_monitor = await module_system.outputs.ConsoleOutput.create(event_bus, listen_topic=Topics.Pipeline.CHUNKER)
+    message_logger = await pipesys.outputs.FileLogger.create(listen_to=conversation_session_processor)
 
+    #visual_output = await pipesys.outputs.VisualOutput.create(listen_to=conversation_session_processor, parent=admin_events)
 
-    visual_output = await module_system.outputs.VisualOutput.create(event_bus, listen_topic=Topics.Pipeline.CONVERSATION_SESSION_REPLY, master=admin_events.window)
-
-    #discord_output = await DiscordOutput.create(event_bus, discord_client, listen_topic=Topics.Router.DISCORD)
-
-    #command_output = await module_system.outputs.CommandOutput.create(event_bus)
+    #discord_output = await DiscordOutput.create(listen_to=conversation_session_processor)
 
     # endregion
 
     print("warming up...")
-    await event_bus.publish(Topics.System.WARMUP)
+    await EventBusSingleton.publish(StartupEvent(StartupStage.WARMUP))
 
     print("linking...")
     # extra linking
 
     print("running!")
-
     # run tasks
     await task_manager.run()
 
-asyncio.run(main())
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    # initialize pyqt5 event loop
+    # this is necessary for compatibility between asyncio and pyqt5
+    app = QApplication([])
+    loop = qasync.QEventLoop(app)
+    asyncio.set_event_loop(loop)
+    with loop:
+        loop.run_until_complete(main())
