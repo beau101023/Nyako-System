fe65fbb6ba910cd56441414c5d5655ad7148150c
beau101023 <bbaer10000@gmail.com>
2025-03-24 19:23:36 -0600

    expose EventParameterFlag in events module

--- event_system/__init__.py
+++ event_system/__init__.py
@@ -1,4 +1,5 @@
 from . import events as events
 from .Event import Event as Event
+from .Event import EventParameterFlag as EventParameterFlag
 from .EventBus import EventBus as EventBus
 from .EventBusSingleton import EventBusSingleton as EventBusSingleton
