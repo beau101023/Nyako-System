4e542f5a736edd83a56174fd1311f297cb0f6c1a
beau101023 <bbaer10000@gmail.com>
2025-05-19 12:50:15 -0600

    import TTS output into the main outputs module

--- pipesys/outputs/__init__.py
+++ pipesys/outputs/__init__.py
@@ -3,7 +3,5 @@ from .DiscordOutput import DiscordOutput as DiscordOutput
 from .DiscordVoiceOutput import DiscordVoiceOutput as DiscordVoiceOutput
 from .FileLogger import FileLogger as FileLogger
 from .PipelineMonitor import PipelineMonitor as PipelineMonitor
+from .TextToSpeechOutput import TextToSpeechOutput as TextToSpeechOutput
 from .VisualOutput import VisualOutput as VisualOutput
-
-## Not imported by default due to need to run VSCode as administrator to use
-# from .TextToSpeechOutput import TextToSpeechOutput
